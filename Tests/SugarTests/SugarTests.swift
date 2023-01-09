e16cc6b963101768d0fe38d592a85d6b513989ce
Paul Tavitian <paul.tavitian@icloud.com>
2023-01-09 13:59:11 +1100

    Initial Commit

---
+++ Tests/SugarTests/SugarTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Sugar
+
+final class SugarTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Sugar().text, "Hello, World!")
+    }
+}

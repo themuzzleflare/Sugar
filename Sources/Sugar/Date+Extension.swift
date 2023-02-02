e0bd5283c1329fa4203dae0466cfd9ee00aa6d1d
Paul Tavitian <paul.tavitian@icloud.com>
2023-02-02 12:23:34 +1100

    changes

--- Sources/Sugar/Date+Extension.swift
+++ Sources/Sugar/Date+Extension.swift
@@ -11,4 +11,21 @@ public extension Date {
   func formatted(using formatter: DateFormatter) -> String {
     return formatter.string(from: self)
   }
+  
+  func difference(in component: Calendar.Component, from other: Date) -> Int? {
+    let (max, min) = orderDate(with: other)
+    let dateComponents = Calendar.current.dateComponents([component], from: min, to: max)
+    return dateComponents.value(for: component)
+  }
+  
+  private func orderDate(with other: Date) -> (Date, Date) {
+    let first = self.timeIntervalSince1970
+    let second = other.timeIntervalSince1970
+    
+    if first >= second {
+      return (self, other)
+    }
+    
+    return (other, self)
+  }
 }
